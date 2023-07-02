8cb69fbc8098e4023d0e2f6645ae4b6ae0eb6a95
kidkidkidkidkid1 <mozambaniac@gmail.com>
2023-07-02 00:55:58 -0700

    Add movement
    Sprint fov does not work properly

--- Assets/Scrips/CamController.cs
+++ Assets/Scrips/CamController.cs
@@ -15,21 +15,23 @@ public class CamController : MonoBehaviour
     private float xRotation;
     private float yRotation;
 
+    [SerializeField] private Transform cameraPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         //center mouse
       Cursor.lockState = CursorLockMode.Locked;
       Cursor.visible = false;
-    
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //mouse input
-        float mouseX = Input.GetAxisRaw("MouseX") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("MouseY") * Time.deltaTime * sensY;
+        //mouse input (x100 is because sens is weird)
+        float mouseX = Input.GetAxisRaw("MouseX") * Time.deltaTime * sensX * 100;
+        float mouseY = Input.GetAxisRaw("MouseY") * Time.deltaTime * sensY * 100;
         //?????????????????
         yRotation += mouseX;
         xRotation -= mouseY;
@@ -38,5 +40,8 @@ public class CamController : MonoBehaviour
         //rotate
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+        //hold position on player using the CameraPosition object transform
+        transform.position = cameraPosition.position;
     }
 }
---
+++ Assets/Scrips/PlayerController.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float sprintModifier;
+    private bool isSprinting;
+
+    [SerializeField] private float groundDrag;
+
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private float airMultiplier;
+    bool readyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask ground;
+    private bool grounded;
+
+    [SerializeField] private Transform orientation;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private Vector3 moveDirection;
+
+    private Rigidbody rb;
+
+    [Header("Camera Settings")]
+    [SerializeField] private new Camera camera;
+    [SerializeField] private float baseFov = 87.5f;
+    [SerializeField] private float sprintFov = 99.9f;
+    [SerializeField] private float zoomTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        readyToJump = true;
+
+        //needed when camera is not serialized(public) to automatically set it, only works when u have 1 camera in the scene
+        //camera = FindObjectOfType<Camera>();
+        camera.fieldOfView = 87.5f;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
+
+        MyInput();
+        SpeedControl();
+
+        //Debug.Log(grounded);
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+
+        //Debug.Log(readyToJump);
+    }
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    /*private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position - new Vector3(0f, playerHeight * 0.5f + 0.2f));
+    } */
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //undo
+        //when to jump
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+
+    private void MovePlayer()
+    {
+        moveDirection = (orientation.forward * verticalInput + (orientation.right * horizontalInput * 0.8f));
+
+        //when grounded
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
+        if (grounded)
+        {
+            if (isSprinting)
+            {
+                Debug.Log("sprinting");
+                StartCoroutine(LerpFov(baseFov, sprintFov));
+                rb.AddForce(moveDirection.normalized * moveSpeed * 10f * sprintModifier, ForceMode.Force);
+
+            }
+            else
+            {
+                Debug.Log("walking");
+                StartCoroutine(LerpFov(sprintFov, baseFov));
+                rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+            }
+
+        }
+
+        //when not grounded
+        else if (!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+    }
+
+    private IEnumerator LerpFov(float startFov, float targetFov)
+    {
+        float currentTime = 0f;
+        while (currentTime < zoomTime)
+        {
+            currentTime += Time.deltaTime;
+            camera.fieldOfView = Mathf.Lerp(startFov, targetFov, currentTime / zoomTime);
+            //Debug.Log("i love cheese and jons and i hate chiken also grande is amazing");
+            yield return null;
+
+        }
+        camera.fieldOfView = targetFov;
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //limit velocidad when needed (character actual movement speed can exceed set speed)
+        if (flatVel.magnitude > moveSpeed && !Input.GetKey(KeyCode.LeftShift))
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        //rest y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
